e36d4f3884cff9baa28fcd37a5445653fc01f9e3
esayas188 <esayasfekade188@gmail.com>
2024-08-29 12:54:57 +0300

    feat(backend): add api for create delete update and list songs

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const songrouter = require("./routes/song");
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/", songrouter);
+
+// Connect to MongoDB
+mongoose
+  .connect(`${process.env.MONGO_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to mongodb..");
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port 4000...");
+    });
+  });
